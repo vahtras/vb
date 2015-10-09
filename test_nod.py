2e6110314aba7b522e07337a8bd9272719a2409b
Olav Vahtras <vahtras@kth.se>
2015-10-09 11:48:14 +0200

    add nod testfile

---
+++ test_nod.py
@@ -0,0 +1,88 @@
+import unittest
+import numpy as np
+from vb import nod, DKL
+from daltools.util.full import init
+
+
+class NodTest(unittest.TestCase):
+
+    def setUp(self):
+        nod.S = init([[1.0, 0.1], [0.1, 1.0]])
+	nod.C = init([[0.7, 0.7], [0.7, -0.7]])
+
+    def tearDown(self):
+        pass
+
+    def test_vac_empty(self):
+	vac = nod([], [])
+	self.assertEqual(vac.electrons(), 0)
+
+    def test_overlap_class_variable(self):
+	one = nod([0], [])
+	np.testing.assert_allclose(one.S, nod.S)
+
+    def test_retrive_alpha_orbitals(self):
+	det = nod([1, 2], [2, 3])
+	self.assertListEqual(det(0), [1, 2])
+
+    def test_retrive_beta_orbitals(self):
+	det = nod([1, 2], [2, 3])
+	self.assertListEqual(det(1), [2, 3])
+
+    def test_repr(self):
+	det = nod([1, 2], [2, 3])
+	self.assertEqual(str(det), '(1 2|2 3)')
+
+    def test_empty_determinant_returns_none(self):
+	det = nod([0], [])
+	self.assertIsNone(det.orbitals()[1])
+
+    def test_alpha_orbitals(self):
+	det = nod([0], [1])
+	alpha_orbitals, _ = det.orbitals()
+	np.testing.assert_allclose(alpha_orbitals, [[.7], [.7]])
+
+    def test_beta_orbitals(self):
+	det = nod([0], [1])
+	_, beta_orbitals = det.orbitals()
+	np.testing.assert_allclose(beta_orbitals, [[.7], [-.7]])
+
+    def test_vac_normalized(self):
+	det = nod([], [])
+	self.assertEqual(det*det, 1)
+
+    def test_alpha_beta_orthogonal(self):
+	alpha = nod([0], [])
+	beta = nod([], [0])
+	self.assertEqual(alpha*beta, 0)
+
+    def test_single_norm(self):
+	alpha = nod([0], [])
+	self.assertEqual(alpha*alpha, 2*0.7*0.77)
+
+    def test_closed_shell_norm(self):
+	sigmag = nod([0], [0])
+	self.assertEqual(sigmag*sigmag, (2*0.7*0.77)**2)
+
+    def test_high_spin_norm(self):
+	sigmagu = nod([0, 1], [])
+	self.assertAlmostEqual(sigmagu*sigmagu, 2*.7*(1+.1)*.7*2*.7*(1-.1)*.7)
+
+    def test_vac_density(self):
+	vac = nod([], [])
+	np.testing.assert_allclose(DKL(vac, vac), [[[0, 0], [0, 0]],  [[0, 0], [0, 0]]])
+
+    def test_alpha_mo_density(self):
+	alpha = nod([0], [])
+	DKL_a, _ = DKL(alpha, alpha, mo=1)
+	np.testing.assert_allclose(DKL_a, [[1./1.078]])
+
+    def test_alpha_ao_density(self):
+	alpha = nod([0], [])
+	DKL_a, _ = DKL(alpha, alpha, mo=0)
+	d_a = 0.49/1.078
+	np.testing.assert_allclose(DKL_a, [[d_a, d_a], [d_a, d_a]])
+
+
+if __name__ == "__main__":
+    unittest.main()
