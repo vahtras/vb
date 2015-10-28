a726039b52145a850b3fe7b4dafa8c6c1f01c93d
Olav Vahtras <vahtras@kth.se>
2015-10-28 14:42:20 +0100

    Split test files

--- test_nod.py
+++ test_nod.py
@@ -287,29 +287,6 @@ class NodPairTest(unittest.TestCase):
         Kd2L_ana = self.K00_L00.right_orbital_hessian()
         np.testing.assert_allclose(Kd2L_num, Kd2L_ana, rtol=DELTA, atol=DELTA)
 
-class StructTest(unittest.TestCase):
-
-    def setUp(self):
-        Nod.S = init([[1.0, 0.1], [0.1, 1.0]])
-        Nod.C = init([[0.7, 0.7], [0.7, -0.7]])
-
-    def tearDown(self):
-        pass
-
-    def test_structure_coefficients_consistent(self):
-        with self.assertRaises(StructError):
-            struct = structure([Nod([0], [0])], [])
-
-    def test_structure_output(self):
-        alpha = Nod([0], [])
-        struct_a = structure([alpha], [1])
-        self.assertEqual(str(struct_a), "1.000000    (0|)")
-
-    def test_structure_ms(self):
-        alpha = Nod([0], [])
-        beta = Nod([], [0])
-        with self.assertRaises(StructError):
-            struct = structure([alpha, beta], [1, 1])
 
 if __name__ == "__main__":
     unittest.main()
---
+++ test_struct.py
@@ -0,0 +1,26 @@
+class StructTest(unittest.TestCase):
+
+    def setUp(self):
+        Nod.S = init([[1.0, 0.1], [0.1, 1.0]])
+        Nod.C = init([[0.7, 0.7], [0.7, -0.7]])
+
+    def tearDown(self):
+        pass
+
+    def test_structure_coefficients_consistent(self):
+        with self.assertRaises(StructError):
+            struct = structure([Nod([0], [0])], [])
+
+    def test_structure_output(self):
+        alpha = Nod([0], [])
+        struct_a = structure([alpha], [1])
+        self.assertEqual(str(struct_a), "1.000000    (0|)")
+
+    def test_structure_ms(self):
+        alpha = Nod([0], [])
+        beta = Nod([], [0])
+        with self.assertRaises(StructError):
+            struct = structure([alpha, beta], [1, 1])
+
+if __name__ == "__main__":
+    unittest.main()
