fb5d95bc8f66196af1f337fb02a90c5db00c6874
Olav Vahtras <vahtras@kth.se>
2016-02-17 07:56:11 +0100

    Make test helper functions inherited class methods

--- test_opt.py
+++ test_opt.py
@@ -4,25 +4,34 @@ import scipy.optimize
 import vb
 import daltools
 
-def wf_energy(coef, wf):
-    wf.coef = coef
-    total_energy = wf.energy() + wf.Z
-    return total_energy
-
-def structure_gradient(coef, wf):
-    wf.coef = coef
-    return wf.energygrad()[0]
-
-def constraint_norm(coef, wf):
-    wf.coef = coef
-    return wf.norm() - 1.0
-
-def constraint_norm_grad(coef, wf):
-    wf.coef = coef
-    return wf.normgrad()[0]
 
 
-class VBTestH2(unittest.TestCase):
+
+
+class VBTestBase(unittest.TestCase):
+
+    @staticmethod
+    def wf_energy(coef, wf):
+        wf.coef = coef
+        total_energy = wf.energy() + wf.Z
+        return total_energy
+
+    @staticmethod
+    def structure_gradient(coef, wf):
+        wf.coef = coef
+        return wf.energygrad()[0]
+
+    @staticmethod
+    def constraint_norm(coef, wf):
+        wf.coef = coef
+        return wf.norm() - 1.0
+
+    @staticmethod
+    def constraint_norm_grad(coef, wf):
+        wf.coef = coef
+        return wf.normgrad()[0]
+
+class VBTestH2(VBTestBase):
 
     def setUp(self):
         self.tmp = os.path.join(os.path.dirname(__file__), 'test_h2_ab')
@@ -48,8 +57,8 @@ class VBTestH2(unittest.TestCase):
 
         self.constraints = (
             {'type': 'eq',
-             'fun': constraint_norm,
-             'jac': constraint_norm_grad,
+             'fun': self.constraint_norm,
+             'jac': self.constraint_norm_grad,
              'args': (self.wf,)
             },
         )
@@ -60,17 +69,60 @@ class VBTestH2(unittest.TestCase):
 
     def test_solver_start_ionic(self):
         result = scipy.optimize.minimize(
-            wf_energy, [1.0, 0.0], jac=structure_gradient, args=(self.wf,),
+            self.wf_energy, [1.0, 0.0], jac=self.structure_gradient, args=(self.wf,),
             constraints=self.constraints, method='SLSQP'
         )
         self.assertAlmostEqual(result.fun, -1.13728383)
 
     def test_solver_start_covalent(self):
         result = scipy.optimize.minimize(
-            wf_energy, [0.0, 1.0], jac=structure_gradient, args=(self.wf,),
+            self.wf_energy, [0.0, 1.0], jac=self.structure_gradient, args=(self.wf,),
             constraints=self.constraints, method='SLSQP'
         )
         self.assertAlmostEqual(result.fun, -1.13728383)
+
+    def test_constraint(self):
+        self.wf.normalize()
+        for c in self.constraints:
+            self.assertAlmostEqual(c['fun'](self.wf.coef, self.wf), 0.0)
+
+class VBTestH2C(VBTestBase):
+
+    def setUp(self):
+        self.tmp = os.path.join(os.path.dirname(__file__), 'test_h2_c')
+        def tmp(fil):
+            return os.path.join(self.tmp, fil)
+
+        vb.Nod.tmpdir = self.tmp
+        vb.Nod.C = daltools.util.full.matrix((10, 2))
+        vb.Nod.C[0, 0] = 1.0
+        vb.Nod.C[5, 1] = 1.0
+        vb.Nod.S = daltools.one.read("OVERLAP", tmp("AOONEINT")).unpack().unblock()
+        self.wf = vb.WaveFunction(
+            [vb.Structure(
+                [vb.Nod([0], [1]), vb.Nod([1], [0])],
+                [1.0, 1.0]
+                ),
+             vb.Structure(
+                [vb.Nod([0], [0]), vb.Nod([1], [1])],
+                [1.0, 1.0]
+                )
+            ],
+            [1.0, 1.0],
+            tmpdir = self.tmp
+        )
+
+        self.constraints = (
+            {'type': 'eq',
+             'fun': self.constraint_norm,
+             'jac': self.constraint_norm_grad,
+             'args': (self.wf,)
+            },
+        )
+        
+
+    def tearDown(self):
+        pass
 
     def test_constraint(self):
         self.wf.normalize()
