744a875ff7f1547e74e923096b522027db827f7f
Olav Vahtras <vahtras@kth.se>
2016-05-23 15:05:34 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="vb",
+    author="Olav Vahtras",
+    author_email="olav.vahtras@gmail.com",
+    version="1.0",
+    url="https://github.com/vahtras/vb",
+    packages=["vb"],
+    )
